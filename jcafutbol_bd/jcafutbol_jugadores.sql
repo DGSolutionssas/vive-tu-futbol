49f31f1da2ea229caa2696e3fcc8c7b375441d00
DGSolutionssas <sebastianmelo@MacBook-Air-de-sebastian.local>
2017-01-14 12:38:36 -0500

    bd jugadores
    bd jugadores

--- jcafutbol_bd/jcafutbol_jugadores.sql
+++ jcafutbol_bd/jcafutbol_jugadores.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 11-01-2017 a las 03:00:16
+-- Tiempo de generación: 14-01-2017 a las 18:36:28
 -- Versión del servidor: 10.1.19-MariaDB
 -- Versión de PHP: 5.6.28
 
@@ -32,10 +32,20 @@ CREATE TABLE `Jugador` (
   `Documento` int(30) NOT NULL,
   `CorreoElectronico` varchar(255) NOT NULL,
   `Celular` int(10) NOT NULL,
-  `DirectorTecnico` tinyint(1) NOT NULL,
-  `Delegado` tinyint(1) NOT NULL,
-  `RepresentanteLegal` tinyint(1) NOT NULL
+  `DirectorTecnico` tinyint(1) NOT NULL DEFAULT '0',
+  `Delegado` tinyint(1) NOT NULL DEFAULT '0',
+  `RepresentanteLegal` tinyint(1) NOT NULL DEFAULT '0',
+  `Url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `Jugador`
+--
+
+INSERT INTO `Jugador` (`IdJugador`, `NombreJugador`, `Documento`, `CorreoElectronico`, `Celular`, `DirectorTecnico`, `Delegado`, `RepresentanteLegal`, `Url`) VALUES
+(1, 'juan', 12345, 'sebastian', 317672637, 1, 0, 0, ''),
+(3, 'sebasia', 1234, 'sebas@as.com', 1234, 1, 0, 0, ''),
+(4, 'julian', 123440, 's@s.c', 1203948390, 0, 1, 0, '');
 
 --
 -- Índices para tablas volcadas
@@ -47,6 +57,15 @@ CREATE TABLE `Jugador` (
 ALTER TABLE `Jugador`
   ADD PRIMARY KEY (`IdJugador`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Jugador`
+--
+ALTER TABLE `Jugador`
+  MODIFY `IdJugador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
